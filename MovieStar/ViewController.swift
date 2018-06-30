8c9896108e4bf6fc0fd0fc9ec4efb32534fc0ecf
Steven Taglohner <taglohner@me.com>
2018-06-30 17:18:31 +0100

    Initial Commit

---
+++ MovieStar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieStar
+//
+//  Created by Steven Taglohner on 30/06/2018.
+//  Copyright © 2018 Steven Taglohner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
